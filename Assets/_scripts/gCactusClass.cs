7227a9660a6c982c000c1a7ba579567b01f6dcf7
Tetta <rafikovaTZ@mail.ru>
2014-12-15 12:54:45 +0600

    cactus

---
+++ Assets/_scripts/gCactusClass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gCactusClass : MonoBehaviour {
+
+
+	private GameObject restart;
+	// Use this for initialization
+	void Start () {
+		restart = GameObject.Find("gui/restart");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collisionObject) {
+		if (collisionObject.gameObject.name == "berry" || collisionObject.gameObject.name == "spider") {
+			restart.SendMessage("OnClick");
+		}
+		
+	}
+}
